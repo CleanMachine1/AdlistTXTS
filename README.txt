b9217ecece8bac67f279aea0afc13fc5f132b719
CleanMachine1 <78213164+CleanMachine1@users.noreply.github.com>
2021-03-17 13:36:02 +0000

    first commit

---
+++ README.txt
@@ -0,0 +1,4 @@
+I do not take ownership of any of these txts, merely using them and having 
+backups here.
+
+Please recommend me more adlists in the issues section! :)
---
+++ fetch.sh
@@ -0,0 +1,6 @@
+rm ./hosts
+rm ./adservers.txt
+
+wget https://raw.githubusercontent.com/anudeepND/blacklist/master/adservers.txt
+wget https://raw.githubusercontent.com/StevenBlack/hosts/master/alternates/fakenews/hosts
+
