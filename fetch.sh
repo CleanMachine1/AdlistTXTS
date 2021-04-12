5f4bc1e4cedce08c2c58147c471535d3c955f882
CleanMachine1 <78213164+CleanMachine1@users.noreply.github.com>
2021-04-12 20:50:25 +0100

    Made it into 1 list

--- fetch.sh
+++ fetch.sh
@@ -1,7 +1,11 @@
 #!/bin/sh
-rm ./dbl
-rm ./hosts
-rm ./adservers.txt
+rm ./list.txt
 wget https://raw.githubusercontent.com/anudeepND/blacklist/master/adservers.txt
 wget https://raw.githubusercontent.com/StevenBlack/hosts/master/alternates/fakenews/hosts
 wget wget https://dbl.oisd.nl/ -O dbl
+cat dbl > list.txt
+cat hosts >> list.txt
+cat adservers.txt >> list.txt
+rm dbl
+rm hosts
+rm adservers.txt
