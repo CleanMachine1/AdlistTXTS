99dbf9c92dee57a29d79490aca6339be309e1403
CleanMachine1 <78213164+CleanMachine1@users.noreply.github.com>
2021-04-12 22:16:16 +0100

    Finished

--- fetch.sh
+++ fetch.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
 rm ./list.txt
+touch list.txt
 wget https://raw.githubusercontent.com/anudeepND/blacklist/master/adservers.txt
 wget https://raw.githubusercontent.com/StevenBlack/hosts/master/alternates/fakenews/hosts
 wget wget https://dbl.oisd.nl/ -O dbl
