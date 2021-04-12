d6bdc497077deaf01bf8aab19d405ee9a0760d9e
CleanMachine1 <78213164+CleanMachine1@users.noreply.github.com>
2021-04-12 10:28:18 +0100

    Added dbl.oisd.nl

--- fetch.sh
+++ fetch.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
+rm ./dbl
 rm ./hosts
 rm ./adservers.txt
 wget https://raw.githubusercontent.com/anudeepND/blacklist/master/adservers.txt
 wget https://raw.githubusercontent.com/StevenBlack/hosts/master/alternates/fakenews/hosts
-
+wget wget https://dbl.oisd.nl/ -O dbl
