b015139ba559b8c5768bd7cc1b7759fa3fe06f5b
CleanMachine1 <78213164+CleanMachine1@users.noreply.github.com>
2021-04-12 22:13:30 +0100

    Changed how fetch.sh works

--- fetch.sh
+++ fetch.sh
@@ -3,9 +3,7 @@ rm ./list.txt
 wget https://raw.githubusercontent.com/anudeepND/blacklist/master/adservers.txt
 wget https://raw.githubusercontent.com/StevenBlack/hosts/master/alternates/fakenews/hosts
 wget wget https://dbl.oisd.nl/ -O dbl
-cat dbl > list.txt
-cat hosts >> list.txt
-cat adservers.txt >> list.txt
+cat hosts adservers.txt dbl > list.txt
 rm dbl
 rm hosts
 rm adservers.txt
