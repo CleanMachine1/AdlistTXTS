7bd60f3a4e3d9e0e43de6e206406d09e6d383e5f
CleanMachine1 <78213164+CleanMachine1@users.noreply.github.com>
2021-04-17 13:08:45 +0100

    Error in refresh script

--- fetch.sh
+++ fetch.sh
@@ -3,7 +3,7 @@ rm ./list.txt
 touch list.txt
 wget https://raw.githubusercontent.com/anudeepND/blacklist/master/adservers.txt
 wget https://raw.githubusercontent.com/StevenBlack/hosts/master/alternates/fakenews/hosts
-wget wget https://dbl.oisd.nl/ -O dbl
+wget https://dbl.oisd.nl/ -O dbl
 cat hosts adservers.txt dbl > list.txt
 rm dbl
 rm hosts
